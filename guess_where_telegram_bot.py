1011b9bc92062706e5f1388a8971d44b8869af77
Berk Erol <berk-erol@outlook.com>
2025-02-20 19:36:21 +0100

    First commit

---
+++ guess_where_telegram_bot.py
@@ -0,0 +1,71 @@
+import logging
+import os
+import random
+import re
+
+import boto3
+
+from botocore.client import Config
+from telegram import Update
+from telegram.ext import ApplicationBuilder, CommandHandler, MessageHandler, filters, ContextTypes
+
+S3_BUCKET = os.environ['S3_BUCKET']
+S3_BUCKET_PREFIX = os.environ['S3_BUCKET_PREFIX']
+AWS_ACCESS_KEY = os.environ['AWS_ACCESS_KEY']
+AWS_SECRET_ACCESS_KEY = os.environ['AWS_SECRET_ACCESS_KEY']
+TELEGRAM_BOT_TOKEN = os.environ['TELEGRAM_BOT_TOKEN']
+
+s3_client = boto3.client('s3', 'eu-central-1', aws_access_key_id=AWS_ACCESS_KEY, aws_secret_access_key=AWS_SECRET_ACCESS_KEY, config=Config(signature_version='s3v4'))
+
+logging.basicConfig(format='%(asctime)s - %(name)s - %(levelname)s - %(message)s', level=logging.INFO)
+
+paginator = s3_client.get_paginator('list_objects_v2')
+files = []
+for page in paginator.paginate(Bucket=S3_BUCKET, Prefix=S3_BUCKET_PREFIX):
+    files.extend([obj['Key'] for obj in page.get('Contents', [])])
+
+
+def extract_location(file_path):
+    file_path = file_path.removeprefix(S3_BUCKET_PREFIX)
+    parts = file_path.split('/')
+    if re.match(r'^\d{4}$', parts[0]) is not None:  # remove the first dir with year if it exists
+        parts = parts[1:]
+    # format: date - order - country - city/**/jpg
+    if ' - ' in parts[0]:
+        return parts[0].split(' - ')[-1]
+    # format: country/city/**/jpg
+    return parts[1]
+
+
+async def send_random_photo(update: Update, context: ContextTypes.DEFAULT_TYPE) -> None:
+    file_key = random.choice(files)
+    location = extract_location(file_key)
+    file_url = s3_client.generate_presigned_url('get_object', Params={'Bucket': S3_BUCKET, 'Key': file_key})
+    logging.info(file_key)
+    logging.info(location)
+    logging.info(file_url)
+
+    context.chat_data['correct_location'] = location.lower()
+    await update.message.reply_photo(photo=file_url, caption='Guess the location!')
+
+
+async def check_location_guess(update: Update, context: ContextTypes.DEFAULT_TYPE):
+    user_guess = update.message.text.strip().lower()
+    correct_location = context.chat_data.get('correct_location', '')
+
+    if not correct_location:
+        await update.message.reply_text('No photo has been sent yet. Use /random first!')
+        return
+
+    if user_guess == correct_location or user_guess in correct_location.split(' '):
+        await update.message.reply_text('✅ Correct!')
+    else:
+        await update.message.reply_text(f"❌ Wrong! The correct location was: {correct_location.title()}")
+
+if __name__ == '__main__':
+    application = ApplicationBuilder().token(TELEGRAM_BOT_TOKEN).build()
+
+    application.add_handler(CommandHandler('random', send_random_photo))
+    application.add_handler(MessageHandler(filters.TEXT & (~filters.COMMAND), check_location_guess))
+
+    application.run_polling()
---
+++ index.md
@@ -0,0 +1,55 @@
+# Guess Where Telegram Bot
+
+Telegram bot that lets you guess the location of a random photo.
+
+## Overview
+
+You can start by sending `/random` command to receive a random photo. Then you can enter your guess and send it as a message to receive feedback (true/false). Made with [python-telegram-bot](https://python-telegram-bot.org/). Inspired by another game of mine [guess-where](https://github.com/berkerol/guess-where).
+
+## Installation & Usage
+
+```sh
+pipenv install
+pipenv shell # without this you can also do: pipenv run python3 guess_where_telegram_bot.py
+python3 guess_where_telegram_bot.py
+```
+
+## Prerequisites
+
+Following environment variables have to be set:
+
+* S3_BUCKET
+* S3_BUCKET_PREFIX
+* AWS_ACCESS_KEY
+* AWS_SECRET_ACCESS_KEY
+* TELEGRAM_BOT_TOKEN
+
+## Photo Structure
+
+Photos can only be read if they are structured in a specific way.
+
+### Directories by city as last directory
+
+`{PATH}/{COUNTRY}/{CITY}/**/{PHOTO}.jpg`
+
+City directory name should be guessed. `{COUNTRY}` **DOESN'T** have to be country, it can be anything. It is just my current setup. `**` in between can contain any number of directories from 0 to N.
+
+### Directory by city as last element
+
+`{PATH}/{DATE} - {ORDER} - {COUNTRY} - {CITY}/**/{PHOTO}.jpg`
+
+City name should be guessed. `{DATE} - {ORDER} - {COUNTRY}` part **DOESN'T** have to be in that structure as long as elements are separated with ` - ` and city is the last element. It is just my current setup. `**` in between can contain any number of directories from 0 to N.
+
+## Photo Source
+
+Photos can be read from S3 bucket.
+
+### S3 Bucket
+
+Use with or without prefix, for example `s3://{BUCKET}` or `s3://{BUCKET}/{PREFIX}/`
+
+There must be at least one directory between bucket and photo directories in your structure, for example `s3://{BUCKET}/Photos/{COUNTRY}/{CITY}/**/{PHOTO}.jpg` or `s3://{BUCKET}/Photos/2022/{DATE} - {ORDER} - {COUNTRY} - {CITY}/**/{PHOTO}.jpg`
+
+## References
+
+* [boto3 S3](https://boto3.amazonaws.com/v1/documentation/api/latest/reference/services/s3.html)
